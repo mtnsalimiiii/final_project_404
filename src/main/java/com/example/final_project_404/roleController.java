d974d78f04cfab07b85874baff579829c55decbe
Amirmahdi Ganji <amir.mahdi.ganji.6656@gmail.com>
2025-06-11 21:16:50 -0700

    role

---
+++ src/main/java/com/example/final_project_404/roleController.java
@@ -0,0 +1,25 @@
+package com.example.final_project_404;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class roleController {
+
+    @FXML
+    private Button studentButton;
+
+    @FXML
+    private Button teacherButton;
+
+    @FXML
+    private Button adminButton;
+
+    @FXML
+    private Button employeeButton;
+
+}
